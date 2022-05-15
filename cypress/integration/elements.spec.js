24aed109c82b0303ba9f0752e8c82cf7c6259e50
EgijaG <egija.graudina@gmail.com>
2022-05-15 18:22:27 +0300

    Final Work task

---
+++ cypress/integration/elements.spec.js
@@ -0,0 +1,47 @@
+import BasePage from "../pageObjects/BasePage";
+import FormPage from "../pageObjects/FormPage";
+import formText from "../fixtures/formText.json";
+
+// Final task in Automation texting
+context("Elements scenarions", () => {
+  context("Automation practice form", () => {
+    it("Form fillings", () => {
+      FormPage.visit();
+      cy.fixture("formText").then((data) => {
+        //   Data input & submit
+        FormPage.firstName.type(data.name);
+        FormPage.lastName.type(data.lastName);
+        FormPage.email.type(data.email);
+        FormPage.controlSelections.contains(data.gender).click();
+        FormPage.userNum.type(data.mobile);
+        FormPage.dateOfBirth.click();
+        FormPage.selectYear.select("1930");
+        FormPage.selectMonth.select("February");
+        FormPage.selectDay.click({ force: true });
+        FormPage.subjectField.type(data.subject + "{enter}");
+        FormPage.controlSelections.contains(data.hobbies).click();
+        FormPage.imgUpload.selectFile(data.imgPath);
+        FormPage.address.type(data.currentAdd);
+        FormPage.stateSelect.type(data.state + "{enter}");
+        FormPage.citySelect.type(data.city + "{enter}");
+        FormPage.submitBtn.click();
+
+        // Data validation
+        FormPage.table
+          .should("contain", data.name)
+          .should("contain", data.lastName)
+          .should("contain", data.email)
+          .should("contain", data.gender)
+          .should("contain", data.mobile)
+          .should("contain", data.subject)
+          .should("contain", data.hobbies)
+          .should("contain", data.currentAdd)
+          .should("contain", data.state)
+          .should("contain", data.city)
+          .should("contain", data.imgPath.split("/")[2])
+          .should("contain", "28 February,1930");
+        FormPage.closeModal.click();
+      });
+    });
+  });
+});
